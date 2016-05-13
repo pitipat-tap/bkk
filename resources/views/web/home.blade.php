1a864455681cbb872ca2ff62c6961c107e17d419
DarkK <intouchxiii@gmail.com>
2016-05-13 18:17:33 +0700

    landing page un fin

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -2,7 +2,6 @@
 
 
 @section("specific_css")
-{!! Html::style("css/home.css") !!}
 @stop
 
 
@@ -14,41 +13,98 @@
 @section("body")
 
 @include("web.top-menu")
-<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-    <ul class="orbit-container">
-        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-        <li class="is-active orbit-slide">
-            <a>
-                <img class="orbit-image" src="http://placehold.it/1500x500" alt="Space">
-                <figcaption class="orbit-caption">1</figcaption>
-            </a>
-        </li>
-        <li class="orbit-slide">
-            <a>
-                <img class="orbit-image" src="http://placehold.it/1500x500" alt="Space">
-                <figcaption class="orbit-caption">2</figcaption>
-            </a>
-        </li>
-        <li class="orbit-slide">
-            <a>
-                <img class="orbit-image" src="http://placehold.it/1500x500" alt="Space">
-                <figcaption class="orbit-caption">3</figcaption>
-            </a>
-        </li>
-    </ul>
-    <div class="orbit-bullets-container">
-        <nav class="orbit-bullets" style="$orbit-bullet-margin-bottom: 0em">
-            <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-            <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-            <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-        </nav>
+
+<section>
+    <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+        <ul class="orbit-container">
+            <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+            <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+            <li class="is-active orbit-slide">
+                <a>
+                    {!! HTML::image("http://placehold.it/1500x500","",array("class"=>"orbit-image")) !!}
+                    <figcaption class="orbit-caption">1</figcaption>
+                </a>
+            </li>
+            <li class="orbit-slide">
+                <a>
+                    {!! HTML::image("http://placehold.it/1500x500","",array("class"=>"orbit-image")) !!}
+                    <figcaption class="orbit-caption">2</figcaption>
+                </a>
+            </li>
+            <li class="orbit-slide">
+                <a>
+                    {!! HTML::image("http://placehold.it/1500x500","",array("class"=>"orbit-image")) !!}
+                    <figcaption class="orbit-caption">3</figcaption>
+                </a>
+            </li>
+        </ul>
+        <div class="orbit-bullets-container">
+            <nav class="orbit-bullets" style="$orbit-bullet-margin-bottom: 0em">
+                <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+                <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+                <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+            </nav>
+        </div>
     </div>
-</div>
-<div class="banner-page-title">Destination</div>
-<div class="body-section">
-    <h1>Body</h1>
-</div>
+</section>
+<section id="home-body">
+    <div class="body-section">
+        <div class="banner-page-title">Destination</div>
+
+        <section class="mg-b-1">
+            <div class="row">
+                <div class="medium-8 columns">
+                      {!! HTML::image("http://placehold.it/800x600","",array("class"=>"")) !!}
+                </div>
+                <div class="medium-4 columns">
+                    <div class="destination-highlight">
+                        <h4> Wat Phra Kaew </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Nam dapibus lectus eget mi aliquam, ac bibendum orci dictum.
+                            Ut malesuada molestie enim, et mattis libero vestibulum id.
+                            Aenean eu pulvinar justo, quis mollis ligula. Nullam commodo odio a luctus blandit.
+                        </p>
+                        {!! HTML::image("http://placehold.it/350x150","",array("class"=>"")) !!}
+                        <p>
+                            Vestibulum at ex convallis, viverra tortor vitae, rutrum ipsum.
+                             Sed quis purus eu velit pretium euismod. 
+                        </p>
+                        <a>
+                            <div> EXPLORE </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="campaign-attractions" >
+            <div class="row">
+                <div class=" medium-4 columns">
+                    <div class="tag-highlight">
+                        <h2> Campaign </h2>
+                        <h1> ___ </h1>
+                    </div>
+                </div>
+                <div class=" medium-8 columns">
+                    {!! HTML::image("http://placehold.it/832x300","",array("class"=>"")) !!}
+                </div>
+            </div>
+            <div class="row">
+                <div class=" medium-8 columns">
+                    {!! HTML::image("http://placehold.it/832x300","",array("class"=>"")) !!}
+                </div>
+                <div class=" medium-4 columns">
+                    <div class="tag-highlight">
+                        <h2> Attractions</h2>
+                        <h1> ___ </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+</section>
 
 @include("web.footer")
 @stop
