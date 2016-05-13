db07e18e34a16c71e7d79f76daf7dc5b983fb9c8
tobestupid <tobestupid@hotmail.com>
2016-05-13 20:42:16 +0700

    Edit footer menu for mobile.

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -1,26 +1,24 @@
 <div class="top-bar footer top-bar-first" align="center">
-  <div class="footer-container">
+    <div class="footer-container">
         <div class="row">
-            <div class="medium-1 column empty">
+            <div class="small-1 medium-1 column empty">
             </div>
-            <div class="medium-4 column">
+            <div class="small-5 medium-4 column">
                 <a href="http://www.agoda.com/" target="_blank">
                     {!! HTML::image("images/logo/agoda-logo.png","",array("class"=>"main-banner")) !!}
                 </a>
             </div>
-            <div class="medium-2 column empty">
+            <div class="hide-for-small-only medium-2 column empty">
             </div>
-            <div class="medium-4 column main-banner-container">
+            <div class="small-5 medium-4 column main-banner-container">
                 <a href="https://www.tripadvisor.com/" target="_blank">
                     {!! HTML::image("images/logo/trip-logo.png","",array("class"=>"main-banner")) !!}
                 </a>
             </div>
-            <div class="medium-1 column empty">
+            <div class="small-1 medium-1 column empty">
             </div>
         </div>
         <hr>
-
-
         <div class="orbit" data-orbit>
             <ul class="orbit-container">
                 <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
@@ -59,62 +57,74 @@
                 </li>
             </ul>
         </div>
-
-
-
-
-  </div>
+    </div>
 </div>
 
-<div class="top-bar footer top-bar-second" align="center">
-  <div class="footer-container">
-    <div class="row medium-up-4 second-footer">
-        <div class="column">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.327923570138!2d100.49096021639426!3d13.759087656345015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xd55c5a3036726e66!2sTourism+Division%3B+Culture%2C+Sports+and+Tourism+Department%3B+BMA!5e0!3m2!1sen!2sth!4v1462784638517" width="100%" height="130" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-        <div class="column">
-            <p class="address">
-                Address : 17/1 Phra Athit Road,<br>
-                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp      Phra Nakhon, Bangkok,<br>
-                 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 10200.<br>
-                Phone  &nbsp&nbsp&nbsp&nbsp: 0-2225-7612-4,<br>
-                Fax     &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: 0-2225-7615-6<br>
-                Email  &nbsp&nbsp&nbsp&nbsp&nbsp: service@bangkoktourist.com
-                
-            </p>
-        </div>
-        <div class="column">
-            <p>
-                <a href="http://203.155.220.167/" target="_blank"><img src="http://www.bangkoktourist.com/site//wp-content/themes/3clicks-child-theme/images/02-Banner252x238.png" width="133" height="130"></a>
-            </p>
-        </div>
-        <div class="column">
-            <div class="fb-like-box fb_iframe_widget" data-href="https://www.facebook.com/tourismdivision" style="height: 160px" data-width="290" data-height="235" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false" fb-xfbml-state="rendered" fb-iframe-plugin-query="app_id=&amp;color_scheme=light&amp;container_width=290&amp;header=false&amp;height=235&amp;href=https%3A%2F%2Fwww.facebook.com%2Ftourismdivision&amp;locale=en_US&amp;sdk=joey&amp;show_border=false&amp;show_faces=true&amp;stream=false&amp;width=290"><span style="vertical-align: bottom; width: 290px; height: 214px;"><iframe name="f3333b5608" width="290px" height="235px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:like_box Facebook Social Plugin" src="https://www.facebook.com/v2.0/plugins/like_box.php?app_id=&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D42%23cb%3Df226a851d8%26domain%3Dwww.bangkoktourist.com%26origin%3Dhttp%253A%252F%252Fwww.bangkoktourist.com%252Ff3bbd6761c%26relation%3Dparent.parent&amp;color_scheme=light&amp;container_width=290&amp;header=false&amp;height=235&amp;href=https%3A%2F%2Fwww.facebook.com%2Ftourismdivision&amp;locale=en_US&amp;sdk=joey&amp;show_border=false&amp;show_faces=true&amp;stream=false&amp;width=290" style="border: none; visibility: visible; width: 300px; height: 200px; transform: scale(0.7);margin-top: -30px;" class=""></iframe></span></div>
+<div class="top-bar footer top-bar-second show-for-medium" align="center">
+    <div class="footer-container">
+        <div class="row medium-up-4 second-footer">
+            <div class="column">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.327923570138!2d100.49096021639426!3d13.759087656345015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xd55c5a3036726e66!2sTourism+Division%3B+Culture%2C+Sports+and+Tourism+Department%3B+BMA!5e0!3m2!1sen!2sth!4v1462784638517" width="100%" height="130" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+            <div class="column">
+                <p class="address">
+                    Address : 17/1 Phra Athit Road,<br>
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp      Phra Nakhon, Bangkok,<br>
+                     &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 10200.<br>
+                    Phone  &nbsp&nbsp&nbsp&nbsp: 0-2225-7612-4,<br>
+                    Fax     &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: 0-2225-7615-6<br>
+                    Email  &nbsp&nbsp&nbsp&nbsp&nbsp: service@bangkoktourist.com
+                    
+                </p>
+            </div>
+            <div class="column">
+                <p>
+                    <a href="http://203.155.220.167/" target="_blank"><img src="http://www.bangkoktourist.com/site//wp-content/themes/3clicks-child-theme/images/02-Banner252x238.png" width="133" height="130"></a>
+                </p>
+            </div>
+            <div class="column">
+                <div class="fb-like-box fb_iframe_widget" data-href="https://www.facebook.com/tourismdivision" style="height: 160px" data-width="290" data-height="235" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false" fb-xfbml-state="rendered" fb-iframe-plugin-query="app_id=&amp;color_scheme=light&amp;container_width=290&amp;header=false&amp;height=235&amp;href=https%3A%2F%2Fwww.facebook.com%2Ftourismdivision&amp;locale=en_US&amp;sdk=joey&amp;show_border=false&amp;show_faces=true&amp;stream=false&amp;width=290"><span style="vertical-align: bottom; width: 290px; height: 214px;"><iframe name="f3333b5608" width="290px" height="235px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:like_box Facebook Social Plugin" src="https://www.facebook.com/v2.0/plugins/like_box.php?app_id=&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D42%23cb%3Df226a851d8%26domain%3Dwww.bangkoktourist.com%26origin%3Dhttp%253A%252F%252Fwww.bangkoktourist.com%252Ff3bbd6761c%26relation%3Dparent.parent&amp;color_scheme=light&amp;container_width=290&amp;header=false&amp;height=235&amp;href=https%3A%2F%2Fwww.facebook.com%2Ftourismdivision&amp;locale=en_US&amp;sdk=joey&amp;show_border=false&amp;show_faces=true&amp;stream=false&amp;width=290" style="border: none; visibility: visible; width: 300px; height: 200px; transform: scale(0.7);margin-top: -30px;" class=""></iframe></span></div>
+            </div>
         </div>
     </div>
-  </div>
+</div>
+
+<div class="top-bar footer top-bar-second show-for-small-only pd-t-1 pd-b-1" align="center">
+    <div class="footer-container">
+        <div class="small-4 column">
+            <a href="https://www.google.com/maps?ll=13.760744,100.49152&z=14&t=m&hl=en&gl=TH&mapclient=embed&cid=15374262391054429798" target="_blank">
+                {!! HTML::image("http://placehold.it/120x60","",array("class"=>"")) !!}
+            </a>
+        </div>
+        <div class="small-4 column">
+            <a href="http://203.155.220.167/" target="_blank">
+                {!! HTML::image("http://placehold.it/120x60","",array("class"=>"")) !!}
+            </a>
+        </div>
+        <div class="small-4 column">
+            <a href="https://m.facebook.com/tourismdivision/" target="_blank">
+                {!! HTML::image("http://placehold.it/120x60","",array("class"=>"")) !!}
+            </a>
+        </div>
+    </div>
 </div>
 
 <div class="top-bar footer copyright-footer" align="center">
-  <div class="footer-container row">
-    <div class="small-12 medium-4 columns align-left">
-        <div class="third-footer">
-            <span>© 2016 by Chapter2</span>
+    <div class="footer-container row">
+        <div class="small-12 medium-4 columns align-left">
+            <div class="third-footer">
+                <span>© 2016 by Chapter2</span>
+            </div>
+            <div class="third-footer">
+                <span>Proudly Created With KiwilauncherTeam</span>
+            </div>
         </div>
-        <div class="third-footer">
-            <span>Proudly Created With KiwilauncherTeam</span>
-        </div>
-    </div>
-    <div class="small-12 medium-8 columns">
-        <div class="small-12 medium-2 medium-push-6 columns">
-            <a href="aboutus">About Us</a>
-        </div>
-        <div class="small-12 medium-2 medium-push-6 columns">
-            <a href="faq">FAQ</a>
-        </div>
-        <div class="small-12 medium-2 columns">
-            <a href="#">Sitemap</a>
+        <div style="float: right">
+            <ul class="vertical medium-horizontal menu">
+                <li><a href="aboutus">About Us</a></li>
+                <li><a href="faq">FAQ</a></li>
+                <li><a href="#">Sitemap</a></li>
+            </ul>
         </div>
     </div>
-  </div>
 </div>
