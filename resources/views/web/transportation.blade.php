089b59891422c2c38259552ef2aa82e5e9a8d6d1
tobestupid <tobestupid@hotmail.com>
2016-05-15 19:22:51 +0700

    Transportation for mobile.

--- resources/views/web/transportation.blade.php
+++ resources/views/web/transportation.blade.php
@@ -19,93 +19,163 @@
 </div>
 <div class="banner-page-title mg-b-1">Transportaion</div>
 <div class="body-section">
-	<div class="transportation-container">
-		{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"mg-b-1")) !!}
 
-		<div class="row">
-			<div class="transportation-label small-3 medium-1 columns">
-				{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
-			</div>
-			<div class="transportation-content small-9 medium-11 columns pd-r-0">
-				<div class="row pd-t-05 pd-l-1">
-					BTS
+	<!-- Desktop -->
+	<div class="show-for-medium columns">
+		<div class="transportation-container">
+			{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"mg-b-1")) !!}
+
+			<div class="row">
+				<div class="transportation-label small-3 medium-1 columns">
+					{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
 				</div>
-				<hr class="mg-1">
-				<div class="row pd-l-1">
-					<div class="small-8 medium-10 columns">
+				<div class="transportation-content small-9 medium-11 columns pd-r-0">
+					<div class="row pd-t-05 pd-l-1">
 						BTS
 					</div>
-					<div class="medium-2 show-for-medium columns">
-						<a class="button mg-b-0" href="http://www.bts.co.th/customer/th/main.aspx" target="_blank">More Info</a>
+					<hr class="mg-1">
+					<div class="row pd-l-1">
+						<div class="small-8 medium-10 columns">
+							BTS
+						</div>
+						<div class="medium-2 show-for-medium columns">
+							<a class="button mg-b-0" href="http://www.bts.co.th/customer/th/main.aspx" target="_blank">More Info</a>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row show-for-small-only columns mg-t-1">
-			<div class="small-12 small-centered columns">
-				<a class="button small-12 columns mg-0" href="http://www.bts.co.th/customer/th/main.aspx" target="_blank">More Info</a>
-			</div>
-		</div>
-	</div>
-	<hr class="mg-b-3 mg-t-1">
-	<div class="transportation-container">
-		{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"mg-b-1")) !!}
+		<hr class="mg-b-3 mg-t-2">
+		<div class="transportation-container">
+			{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"mg-b-1")) !!}
 
-		<div class="row">
-			<div class="transportation-label small-3 medium-1 columns">
-				{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
-			</div>
-			<div class="transportation-content small-9 medium-11 columns pd-r-0">
-				<div class="row pd-t-05 pd-l-1">
-					MRT
+			<div class="row">
+				<div class="transportation-label small-3 medium-1 columns">
+					{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
 				</div>
-				<hr class="mg-1">
-				<div class="row pd-l-1">
-					<div class="small-8 medium-10 columns">
+				<div class="transportation-content small-9 medium-11 columns pd-r-0">
+					<div class="row pd-t-05 pd-l-1">
 						MRT
 					</div>
-					<div class="medium-2 show-for-medium columns">
-						<a class="button mg-b-0" href="http://www.bangkokmetro.co.th/index.aspx?Lang=En" target="_blank">More Info</a>
+					<hr class="mg-1">
+					<div class="row pd-l-1">
+						<div class="small-8 medium-10 columns">
+							MRT
+						</div>
+						<div class="medium-2 show-for-medium columns">
+							<a class="button mg-b-0" href="http://www.bangkokmetro.co.th/index.aspx?Lang=En" target="_blank">More Info</a>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row show-for-small-only columns mg-t-1">
-			<div class="small-12 small-centered columns">
-				<a class="button small-12 columns mg-0" href="http://www.bangkokmetro.co.th/index.aspx?Lang=En" target="_blank">More Info</a>
-			</div>
-		</div>
-	</div>
-	<hr class="mg-b-3 mg-t-1">
-	<div class="transportation-container">
-		{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"mg-b-1")) !!}
+		<hr class="mg-b-3 mg-t-2">
+		<div class="transportation-container">
+			{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"mg-b-1")) !!}
 
-		<div class="row">
-			<div class="transportation-label small-3 medium-1 columns">
-				{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
-			</div>
-			<div class="transportation-content small-9 medium-11 columns pd-r-0">
-				<div class="row pd-t-05 pd-l-1">
-					BMTA
+			<div class="row">
+				<div class="transportation-label small-3 medium-1 columns">
+					{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
 				</div>
-				<hr class="mg-1">
-				<div class="row pd-l-1">
-					<div class="small-8 medium-10 columns">
+				<div class="transportation-content small-9 medium-11 columns pd-r-0">
+					<div class="row pd-t-05 pd-l-1">
 						BMTA
 					</div>
-					<div class="medium-2 show-for-medium columns">
-						<a class="button mg-b-0" href="http://www.bmta.co.th/?q=th/home" target="_blank">More Info</a>
+					<hr class="mg-1">
+					<div class="row pd-l-1">
+						<div class="small-8 medium-10 columns">
+							BMTA
+						</div>
+						<div class="medium-2 show-for-medium columns">
+							<a class="button mg-b-0" href="http://www.bmta.co.th/?q=th/home" target="_blank">More Info</a>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row show-for-small-only columns mg-t-1">
-			<div class="small-12 small-centered columns">
-				<a class="button small-12 columns mg-0" href="http://www.bmta.co.th/?q=th/home" target="_blank">More Info</a>
+		<hr class="mg-b-3 mg-t-1">
+	</div>
+
+	<!-- Mobile -->
+	<div class="show-for-small-only">
+		<div class="content-box">
+			<div class="transportation-container">
+				{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"transportation-picture-mobile")) !!}
+
+				<div>
+					<div class="transportation-label small-3 columns">
+						{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
+					</div>
+					<div class="transportation-content small-9 columns">
+						<div class="row">
+							BTS
+						</div>
+						<hr>
+						<div class="row">
+							BTS
+						</div>
+					</div>
+				</div>
+				<div class="row show-for-small-only columns mg-t-1">
+					<div class="small-12 small-centered columns">
+						<a class="button small-12 columns mg-0" href="http://www.bmta.co.th/?q=th/home" target="_blank">More Info</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr class="mg-b-1 mg-t-1">
+		<div class="content-box">
+			<div class="transportation-container">
+				{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"transportation-picture-mobile")) !!}
+
+				<div>
+					<div class="transportation-label small-3 columns">
+						{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
+					</div>
+					<div class="transportation-content small-9 columns">
+						<div class="row">
+							MRT
+						</div>
+						<hr>
+						<div class="row">
+							MRT
+						</div>
+					</div>
+				</div>
+				<div class="row show-for-small-only columns mg-t-1">
+					<div class="small-12 small-centered columns">
+						<a class="button small-12 columns mg-0" href="http://www.bmta.co.th/?q=th/home" target="_blank">More Info</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr class="mg-b-1 mg-t-1">
+		<div class="content-box mg-b-2">
+			<div class="transportation-container">
+				{!! HTML::image("http://placehold.it/1248x416","bkk-logo",array("class"=>"transportation-picture-mobile")) !!}
+
+				<div>
+					<div class="transportation-label small-3 columns">
+						{!! HTML::image("http://placehold.it/120x120","bkk-logo",array("class"=>"")) !!}
+					</div>
+					<div class="transportation-content small-9 columns">
+						<div class="row">
+							BMTA
+						</div>
+						<hr>
+						<div class="row">
+							BMTA
+						</div>
+					</div>
+				</div>
+				<div class="row show-for-small-only columns mg-t-1">
+					<div class="small-12 small-centered columns">
+						<a class="button small-12 columns mg-0" href="http://www.bmta.co.th/?q=th/home" target="_blank">More Info</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-	<hr class="mg-b-3 mg-t-1">
 </div>
 @include("web.footer")
 @stop
