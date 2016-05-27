e5ac119456ad66246db5a5f967a2b892eecc627d
tobestupid <tobestupid@hotmail.com>
2016-05-27 15:34:49 +0700

    Use javascript to set page banner.

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -20,13 +20,6 @@ $(document).ready(function() {
       localStorage.setItem('language', selectedLanguage);
   });
 
-	var path = window.location.href;
-	var loc = path.substring(path.lastIndexOf('/')+1);
-
-	if(loc=="partners") {
-		document.getElementById("partners").style.display = "none";
-	}
-
 	// mobile menu
 	$('#button-menu-mobile').click(function(){
 		$(this).toggleClass('open');
@@ -36,4 +29,24 @@ $(document).ready(function() {
 			$('#button-menu-mobile').removeClass('open');
 		}
 	});
+
+    function setPageBanner() {
+        // partner page
+        var path = window.location.href;
+        var loc = path.substring(path.lastIndexOf('/')+1);
+
+        if(loc=="partners") {
+            document.getElementById("partners").style.display = "none";
+        }
+
+        if (Foundation.MediaQuery.atLeast('large')) {
+            document.getElementById('banner-img').src = "http://placehold.it/1500x500";
+        } else if (Foundation.MediaQuery.atLeast('medium')) {
+            document.getElementById('banner-img').src = "http://placehold.it/400x300";
+        } else {
+            document.getElementById('banner-img').src = "http://placehold.it/300x400";
+        }
+    }
+
+    setPageBanner();
 });
