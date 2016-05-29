0217684329f898accb98e1806e1bf5515652760b
tobestupid <tobestupid@hotmail.com>
2016-05-29 22:17:23 +0700

    Crop banner for mobile and tablet.

--- resources/views/web/banner-orbit.blade.php
+++ resources/views/web/banner-orbit.blade.php
@@ -33,15 +33,17 @@
     <!-- tablet -->
     <div class="orbit show-for-medium-only columns mg-b-1" role="region" aria-label="Favorite Space Pictures" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
         <ul class="orbit-container">
-            <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-            <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
             @foreach( $banners as $banner)
-                <li class="orbit-slide">
-                    <a>
-                        {!! Html::image($banner->image_url, 'alt', array("class"=>"orbit-image")) !!}
-                        <figcaption class="orbit-caption">{!! $banner->caption !!}</figcaption>
-                    </a>
-                </li>
+                @if($banner->sequence == 1)
+                    <li class="is-active orbit-slide">
+                @else
+                    <li class="orbit-slide">
+                @endif
+                        <a>
+                            {!! Html::image($banner->image_url, 'alt', array("class"=>"orbit-image")) !!}
+                            <figcaption class="orbit-caption">{!! $banner->caption !!}</figcaption>
+                        </a>
+                    </li>
             @endforeach
         </ul>
     </div>
@@ -49,15 +51,17 @@
     <!-- mobile -->
     <div class="orbit show-for-small-only columns mg-b-1" role="region" aria-label="Favorite Space Pictures" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
         <ul class="orbit-container">
-            <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-            <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
             @foreach( $banners as $banner)
-                <li class="orbit-slide">
-                    <a>
-                        {!! Html::image($banner->image_url, 'alt', array("class"=>"orbit-image")) !!}
-                        <figcaption class="orbit-caption">{!! $banner->caption !!}</figcaption>
-                    </a>
-                </li>
+                @if($banner->sequence == 1)
+                    <li class="is-active orbit-slide">
+                @else
+                    <li class="orbit-slide">
+                @endif
+                        <a>
+                            {!! Html::image($banner->image_url, 'alt', array("class"=>"orbit-image")) !!}
+                            <figcaption class="orbit-caption">{!! $banner->caption !!}</figcaption>
+                        </a>
+                    </li>
             @endforeach
         </ul>
     </div>
