b4c14d5842985b7a431caa132a783afa816f505d
tobestupid <tobestupid@hotmail.com>
2016-05-30 20:20:56 +0700

    Delete banner in js.

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -30,23 +30,11 @@ $(document).ready(function() {
 		}
 	});
 
-    function setPageBanner() {
-        // partner page
-        var path = window.location.href;
-        var loc = path.substring(path.lastIndexOf('/')+1);
+  // partner page
+  var path = window.location.href;
+  var loc = path.substring(path.lastIndexOf('/')+1);
 
-        if(loc=="partners") {
-            document.getElementById("partners").style.display = "none";
-        }
-
-        if (Foundation.MediaQuery.atLeast('large')) {
-            document.getElementById('banner-img').src = "http://placehold.it/1500x500";
-        } else if (Foundation.MediaQuery.atLeast('medium')) {
-            document.getElementById('banner-img').src = "http://placehold.it/400x300";
-        } else {
-            document.getElementById('banner-img').src = "http://placehold.it/300x400";
-        }
-    }
-
-    setPageBanner();
+  if(loc=="partners") {
+      document.getElementById("partners").style.display = "none";
+  }
 });
