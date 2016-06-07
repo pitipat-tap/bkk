dbaf61abd19b60868790c510f9205f4fd0f92904
soranad <oldman.beta@gmail.com>
2016-06-07 15:36:05 +0700

    add update db

---
+++ database/migrations/2016_06_07_082044_fix_attractions_table_2.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixAttractionsTable2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('attractions', function($table) {
+            $table->text('telephone_number');
+            $table->text('website');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('attractions', function($table) {
+            $table->dropColumn('telephone_number');
+            $table->dropColumn('website');
+        });
+    }
+}
