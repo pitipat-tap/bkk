da62913b86638dcc1ca188ba8851563a869797e8
tobestupid <tobestupid@hotmail.com>
2016-06-12 14:44:17 +0700

    Edit optimize images level.

--- gulpfile.js
+++ gulpfile.js
@@ -62,14 +62,14 @@ gulp.task('compile', function () {
 //optimize image 
 gulp.task('images', function(cb) {
     gulp.src(['public/images/**/*.png','public/images/**/*.jpg','public/images/**/*.gif','public/images/**/*.jpeg']).pipe(imageop({
-        optimizationLevel: 5,
+        optimizationLevel: 9,
         progressive: true,
         interlaced: true
     })).pipe(gulp.dest('public/images')).on('end', cb).on('error', cb);
 });
 gulp.task('images-upload', function(cb) {
     gulp.src(['public/uploads/normal/**/*.png','public/uploads/normal/**/*.jpg','public/uploads/normal/**/*.gif','public/uploads/normal/**/*.jpeg']).pipe(imageop({
-        optimizationLevel: 5,
+        optimizationLevel: 9,
         progressive: true,
         interlaced: true
     })).pipe(gulp.dest('public/uploads/normal')).on('end', cb).on('error', cb);
